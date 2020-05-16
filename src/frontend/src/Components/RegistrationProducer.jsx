5891f690ce8df533da08567e6cd0315b7aefa3a7
Roman Štěpánek <st52565@student.upce.cz>
2020-05-16 19:55:00 +0200

    edit registration producer

--- src/frontend/src/Components/RegistrationProducer.jsx
+++ src/frontend/src/Components/RegistrationProducer.jsx
@@ -6,30 +6,22 @@ export class RegistrationProducer extends React.Component {
     constructor() {
         super();
         this.state = {
-            name: "",
-            email: "",
-            phone:"",
-            password: "",
-            city: "",
-            postalCode:"",
-            street: "",
-            houseNumber: ""
+            producer: {
+                name: "",
+                email: "",
+                phone:"",
+                password: "",
+                city: "",
+                postalCode:"",
+                street: "",
+                houseNumber: ""
+            }
         }
     }
 
     onButtonRegistration = () => {
-        const name = this.state.name;
-        const email = this.state.email;
-        const phone = this.state.phone;
-        const password = this.state.password;
-        const city = this.state.city;
-        const postalCode = this.state.postalCode;
-        const street = this.state.street;
-        const houseNumber = this.state.houseNumber;
-        const producer = {name, email, phone, password, city, postalCode, street, houseNumber};
-
         let url = 'http://localhost:8080/registrationProducer';
-        FetchUtil.fetchPost(url, JSON.stringify(this.state.name))
+        FetchUtil.fetchPost(url, JSON.stringify(this.state.producer))
             .then(response => response.json())
             .then(data => {
                 console.log(data)
@@ -46,43 +38,43 @@ export class RegistrationProducer extends React.Component {
                 <h3>Registration Producer</h3>
                 <div>
                     <label htmlFor="producer-name">Name producer</label>
-                    <input type="text" name="producer-name" value={this.state.name}
+                    <input type="text" name="producer-name" value={this.state.producer.name}
                            onChange={(evt) => this.onChangeHandler(evt, 'name')}/>
                 </div>
                 <div>
                     <label htmlFor="producer-email">e-mail</label>
-                    <input type="text" name="producer-email" value={this.state.email}
+                    <input type="text" name="producer-email" value={this.state.producer.email}
                            onChange={(evt) => this.onChangeHandler(evt, 'email')}/>
                 </div>
                 <div>
                     <label htmlFor="user-phone">Phone</label>
-                    <input type="text" name="producer-phone" value={this.state.phone}
+                    <input type="text" name="producer-phone" value={this.state.producer.phone}
                            onChange={(evt) => this.onChangeHandler(evt, 'phone')}/>
                 </div>
                 <div>
                     <label htmlFor="producer-password">Password</label>
-                    <input type="password" name="producer-password" value={this.state.password}
+                    <input type="password" name="producer-password" value={this.state.producer.password}
                            onChange={(evt) => this.onChangeHandler(evt, 'password')}/>
                 </div>
                 <div>
                     <label htmlFor="producer-city">City</label>
-                    <input type="text" name="producer-city" value={this.state.city}
+                    <input type="text" name="producer-city" value={this.state.producer.city}
                            onChange={(evt) => this.onChangeHandler(evt, 'city')}/>
                 </div>
 
                 <div>
                     <label htmlFor="user-postalCode">Postal code</label>
-                    <input type="number" name="producer-postalCode" value={this.state.postalCode}
+                    <input type="number" name="producer-postalCode" value={this.state.producer.postalCode}
                            onChange={(evt) => this.onChangeHandler(evt, 'postalCode')}/>
                 </div>
                 <div>
                     <label htmlFor="producer-street">Street</label>
-                    <input type="text" name="producer-street" value={this.state.street}
+                    <input type="text" name="producer-street" value={this.state.producer.street}
                            onChange={(evt) => this.onChangeHandler(evt, 'street')}/>
                 </div>
                 <div>
                     <label htmlFor="producer-houseNumber">House number</label>
-                    <input type="number" name="producer-houseNumber" value={this.state.houseNumber}
+                    <input type="number" name="producer-houseNumber" value={this.state.producer.houseNumber}
                            onChange={(evt) => this.onChangeHandler(evt, 'houseNumber')}/>
                 </div>
                 <button onClick={this.onButtonRegistration}>Registration</button>
